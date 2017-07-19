7f281f9ca284c43f15aad05243a1fbd5048b5194
Muzhilkin Alexander <amuzhilkin@gmail.com>
2017-07-20 00:35:06 +0300

    no message

--- assets/static/pages/treeBreeds/js/treeBreeds.js
+++ assets/static/pages/treeBreeds/js/treeBreeds.js
@@ -1,10 +1,17 @@
 (function ($) {
-    var windowBrowser = $(window);
+    var windowBrowser = $(window),
+        sosnaOffset = $('#sosna').offset().top,
+        listvenniczaOffset = $('#listvennicza').offset().top,
+        dubOffset = $('#dub').offset().top,
+        fixedBlock = $('.fixed__block'),
+        fixedBlockOffset = fixedBlock.offset().top;
+
     windowBrowser.ready(function() {
         if (windowBrowser.width() < 745) {
             addSwiper();
         }
     });
+
     windowBrowser.resize(function() {
         if (windowBrowser.width() < 745) {
             addSwiper();
@@ -12,7 +19,7 @@
         else {
             $('.main').removeClass('swiper-container');
             $('.columns__wrapper').removeClass('swiper-wrapper');
-            $('.breeds').removeClass('swiper-slide');
+            $('.breed').removeClass('swiper-slide');
             $('.swiper__btn').addClass('hidden');
         }
     });
@@ -20,7 +27,7 @@
     function addSwiper () {
         $('.main').addClass('swiper-container');
         $('.columns__wrapper').addClass('swiper-wrapper');
-        $('.breeds').addClass('swiper-slide');
+        $('.breed').addClass('swiper-slide');
         $('.swiper__btn').removeClass('hidden');
         var swiper = new Swiper('.swiper-container', {
             nextButton: $('.specification__table .swiper-button-next'),
@@ -30,8 +37,25 @@
 
 
     windowBrowser.scroll(function() {
-        var scrollTop = $(this).scrollTop();
-        if(scrollTop > $('.listvennicza').offset().top) {
+        var windowTop = $(this).scrollTop(),
+            bottomFixed = windowTop + fixedBlockOffset + fixedBlock.height(),
+            bottomFixedContainer = fixedBlockOffset + $('.fixed__container__wrapper').height();
+
+        if (windowTop < fixedBlockOffset) {
+            fixedBlock.removeClass('with__sticky__menu');
+        }
+        else {
+            fixedBlock.addClass('with__sticky__menu');
+            if (bottomFixed >= bottomFixedContainer) {
+                fixedBlock.addClass('bottom');
+                fixedBlock.removeClass('with__sticky__menu');
+            } else {
+                fixedBlock.removeClass('bottom');
+                fixedBlock.addClass('with__sticky__menu');
+            }
+        }
+
+        if(windowTop > $('.listvennicza').offset().top) {
             $('.current[class != hidden]').addClass('hidden');
             $('.listvennicza__current').removeClass('hidden');
         } else {
