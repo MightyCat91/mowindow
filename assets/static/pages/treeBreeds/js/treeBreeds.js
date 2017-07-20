a05ef2987b0109e2d8f80837992c122d40fdec60
muzhilkin <muzhilkin@krista.ru>
2017-07-20 17:20:03 +0300

    no message

--- assets/static/pages/treeBreeds/js/treeBreeds.js
+++ assets/static/pages/treeBreeds/js/treeBreeds.js
@@ -1,26 +1,37 @@
 (function ($) {
     var windowBrowser = $(window),
-        sosnaOffset = $('#sosna').offset().top,
-        listvenniczaOffset = $('#listvennicza').offset().top,
-        dubOffset = $('#dub').offset().top,
+        sosnaBlock = $('.sosna'),
+        listvenniczaBlock = $('.listvennicza'),
+        dubBlock = $('.dub'),
+        fixedContainer = $('.fixed__container__wrapper'),
         fixedBlock = $('.fixed__block'),
         fixedBlockOffset = fixedBlock.offset().top;
 
     windowBrowser.ready(function() {
-        if (windowBrowser.width() < 745) {
+        if (windowBrowser.width() < 992) {
             addSwiper();
+            fixedContainer.addClass('mobile');
+        }
+        if (fixedContainer.hasClass('mobile')) {
+            if (windowBrowser.scrollTop() > fixedBlockOffset) {
+                fixedContainer.addClass('fixed');
+            } else {
+                fixedContainer.removeClass('fixed');
+            }
         }
     });
 
     windowBrowser.resize(function() {
-        if (windowBrowser.width() < 745) {
+        if (windowBrowser.width() < 992) {
             addSwiper();
+            fixedContainer.addClass('mobile');
         }
         else {
             $('.main').removeClass('swiper-container');
             $('.columns__wrapper').removeClass('swiper-wrapper');
             $('.breed').removeClass('swiper-slide');
             $('.swiper__btn').addClass('hidden');
+            fixedContainer.removeClass('mobile');
         }
     });
 
@@ -35,23 +46,52 @@
         })
     }
 
-
     windowBrowser.scroll(function() {
         var windowTop = $(this).scrollTop(),
             bottomFixed = windowTop + fixedBlockOffset + fixedBlock.height(),
-            bottomFixedContainer = fixedBlockOffset + $('.fixed__container__wrapper').height();
+            bottomFixedContainer = fixedBlockOffset + $('.fixed__container__wrapper').height(),
+            sosnaBlockOffset = sosnaBlock.offset().top,
+            sosnaBlockHeight = sosnaBlock.height(),
+            listvenniczaBlockOffset = listvenniczaBlock.offset().top,
+            listvenniczaBlockHeight = listvenniczaBlock.height(),
+            dot = [1/5,2/5,3/5,4/5,1];
 
-        if (windowTop < fixedBlockOffset) {
-            fixedBlock.removeClass('with__sticky__menu');
-        }
-        else {
-            fixedBlock.addClass('with__sticky__menu');
-            if (bottomFixed >= bottomFixedContainer) {
-                fixedBlock.addClass('bottom');
-                fixedBlock.removeClass('with__sticky__menu');
+
+        if (fixedContainer.hasClass('mobile')) {
+            if (windowTop > fixedBlockOffset) {
+                fixedContainer.addClass('fixed');
             } else {
-                fixedBlock.removeClass('bottom');
+                fixedContainer.removeClass('fixed');
+            }
+        } else {
+            console.log('1');
+            if (windowTop < 151) {
+                fixedBlock.removeClass('with__sticky__menu');
+            }
+            else {
                 fixedBlock.addClass('with__sticky__menu');
+
+                if (bottomFixed >= bottomFixedContainer) {
+                    fixedBlock.addClass('bottom');
+                    fixedBlock.removeClass('with__sticky__menu');
+                } else {
+                    fixedBlock.removeClass('bottom');
+                    fixedBlock.addClass('with__sticky__menu');
+                }
+            }
+        }
+
+        $(dot).each(function (index, element){
+            dotScrolled('sosna', sosnaBlockOffset, sosnaBlockHeight, index, element);
+            dotScrolled('listvennicza', listvenniczaBlockOffset, listvenniczaBlockHeight, index, element);
+        });
+
+        function dotScrolled(selectorClass, blockOffset, blockHeight, index, ratio) {
+            console.log(selectorClass, blockOffset, blockHeight, index, ratio);
+            if (windowTop > (blockOffset + blockHeight * ratio)) {
+                $('.' + selectorClass + '__doted .dot:eq(' + index + ')').addClass('scrolled');
+            } else {
+                $('.' + selectorClass + '__doted .dot:eq(' + index + ')').removeClass('scrolled');
             }
         }
 
@@ -62,6 +102,5 @@
             $('.current[class != hidden]').addClass('hidden');
             $('.sosna__current').removeClass('hidden');
         }
-
     });
 })(jQuery);
