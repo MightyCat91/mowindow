f0521c50924536a5a00bb62380e14ed441031bf5
Muzhilkin Alexander <amuzhilkin@gmail.com>
2017-07-22 11:01:16 +0300

    steclopakety

---
+++ assets/static/pages/steclopakety/js/steclopakety.js
@@ -0,0 +1,24 @@
+(function ($) {
+    //переключение изображений и текста по клику по toggle-кнопке
+    $('.switch input').on('click', function () {
+        $('.snow').toggleClass('hidden');
+        $('.sun').toggleClass('hidden');
+        $('.snow__text').toggleClass('hidden');
+        $('.sun__text').toggleClass('hidden');
+        $('.window__sun').toggleClass('hidden');
+        $('.window__snow').toggleClass('hidden');
+    });
+
+    //если ширина окна менее 992рх
+    if ($(window).width() < 992) {
+        //инициализируем слайдер для таблицы
+        $('.main').addClass('swiper-container');
+        $('.columns__wrapper').addClass('swiper-wrapper');
+        $('.breed').addClass('swiper-slide');
+        $('.swiper__btn').removeClass('hidden');
+        new Swiper('.swiper-container', {
+            nextButton: $('.specification__table .swiper-button-next'),
+            prevButton: $('.specification__table .swiper-button-prev')
+        });
+    }
+})(jQuery);
