04852289fc4f69d168c61e78e2b61862890cd79e
Muzhilkin Alexander <amuzhilkin@gmail.com>
2017-07-24 23:51:43 +0300

    profileSystem

--- assets/static/pages/profileSystems/js/profileSystems.js
+++ assets/static/pages/profileSystems/js/profileSystems.js
@@ -14,11 +14,8 @@
     //смещение блока навигации относительно начала страницы
         fixedBlockOffset = fixedBlock.offset().top;
 
-    $('.fancy').fancybox();
-    fixedContainer.height($('.section__2').height());
-
     //если ширина окна менее 992рх
-    if (windowBrowser.width() < 992) {
+    if (window.innerWidth < 992) {
         //инициализируем слайдер для таблицы
         $('.main').addClass('swiper-container');
         $('.columns__wrapper').addClass('swiper-wrapper');
@@ -32,14 +29,17 @@
         fixedContainer.addClass('mobile');
     }
 
+
     //если происходит скролл страницы
     windowBrowser.scroll(function () {
         //смещение скролла
         var windowTop = $(this).scrollTop(),
+        //контейнер контента, к которому прявазан блок быстрой навигации
+            sectionHeight = $('.wrapper'),
         //нижняя граница блока быстрой навигации
             bottomFixed = windowTop + fixedBlock.height(),
         //нижняя граница контейнера блока быстрой навигации - отступ
-            bottomFixedContainer = fixedContainer.offset().top + fixedContainer.height() - 60,
+            bottomFixedContainer = sectionHeight.offset().top + sectionHeight.height() - 60,
         //смещение блока "Стандарт" относительно начала страницы
             block__1__Offset = block__1.offset().top,
         //высота блока "Стандарт"
@@ -49,7 +49,7 @@
         //высота блока "Алю"
             block__2__Height = block__2.height(),
         //части высоты блоков "Стандарт", "Алю", "Классик" соответсвующие точкам в блоке быстрой навигации
-            dot = [1/5, 2/5, 3/5, 4/5, 1];
+            dot = [0, 1/5, 2/5, 3/5, 4/5, 1];
 
         //если текущее разрешение - мобильное
         if (fixedContainer.hasClass('mobile')) {
@@ -62,7 +62,7 @@
                 fixedContainer.removeClass('fixed');
             }
         } else {
-            console.log(bottomFixed,fixedBlockOffset, bottomFixedContainer);
+            fixedContainer.height(sectionHeight.height());
             //если скролл страницы меньше высоты меню + высоты первого региона с формой + 1рх(для отсутсвия скачков)
             if (windowTop < 430) {
                 fixedBlock.removeClass('fixed');
@@ -79,6 +79,13 @@
                     fixedBlock.removeClass('bottom');
                 }
             }
+            //if (windowTop >= $('.profile__1 .image').offset().top - $('.main__info').height()) {
+            //    $('.profile__1 .image').addClass('fixed');
+            //    if (windowTop >= $('.profile__1 .image').offset().top - $('.main__info').height() +
+            //        $('.profile__1 .image').height()) {
+            //        $('.profile__1 .image').removeClass('fixed').addClass('bottom');
+            //    }
+            //}
         }
 
         //для каждой части высоты блоков "сосна","лиственница","дуб"
@@ -102,9 +109,15 @@
             // высоты окна
             if (windowTop > (blockOffset + blockHeight * ratio - windowBrowser.height() / 2)) {
                 $('.' + selectorClass + '__doted .dot:eq(' + index + ')').addClass('scrolled');
+                imgScrolled(selectorClass, index+1);
             } else {
                 $('.' + selectorClass + '__doted .dot:eq(' + index + ')').removeClass('scrolled');
             }
+        }
+
+        function imgScrolled(selectorClass, index) {
+            $('.' + selectorClass + ' .image img').addClass('hidden');
+            $('.' + selectorClass + ' .image img:eq(' + index + ')').removeClass('hidden');
         }
 
         //если текущий скролл больше смещения блока "Классик" за вычетом половины высоты окна
